279ff7e95f5dbc9662ee24b3c346be261419a926
Ganesh Kanna <mail2sgk@gmail.com>
2021-02-20 00:21:00 +0530

    Update ec2_basic_user_data.sh

--- ec2_basic_user_data.sh
+++ ec2_basic_user_data.sh
@@ -10,7 +10,7 @@ sudo su
 
 # install httpd (Linux 2 version)
 yum update -y
-yum install -y httpd .x86_64
+yum install -y httpd.x86_64
 systemctl start httpd.service
 systemctl enable httpd.service
 echo "Hello world from $(hostname -f)" > /var/www/html/index.html
